4400145e87e297f987fc4dc99165eb70944cdac0
rpletcher <ryan.pletcher@gmail.com>
2014-01-27 23:36:05 -0700

    Version 1.2.4
    1.2.4
    FIX: Removed a var_dump accidentally left in the code
    1.2.3
    FIX: Paypal now will get Giftcard Data on the receipt as a discount
    FIX: Undefined index on order page when no giftcard is present has been
    fixed

---
+++ includes/giftcard-actions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Add Giftcard Page
+ *
+ * @package     Woocommerce
+ * @subpackage  Admin/Giftcards
+ * @copyright   Copyright (c) 2014, Ryan Pletcher
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+ */
+
+
+	
