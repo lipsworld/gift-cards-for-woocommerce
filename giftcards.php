dba4bc54bdc42a160f085ff0e6ee5dca72960965
rpletcher <ryan.pletcher@gmail.com>
2014-03-13 22:00:24 -0700

    Commit Version 1.3.2

---
+++ giftcards.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: Gift Cards for WooCommerce
+Plugin URI: http://ryanpletcher.com
+Description: Gift Cards for WooCommerce allows you to offer gift cards to your customer and allow them to place orders using them.
+Version: 1.3.2
+Author: Ryan Pletcher
+Author URI: http://ryanpletcher.com
+License: GPL2
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Plugin version
+if ( ! defined( 'RPWCGC_VERSION' ) )
+	define( 'RPWCGC_VERSION', '1.3.2' );
+
+// Plugin Folder Path
+if ( ! defined( 'RPWCGC_PATH' ) )
+	define( 'RPWCGC_PATH', plugin_dir_path( __FILE__ ) );
+
+// Plugin Folder URL
+if ( ! defined( 'RPWCGC_URL' ) )
+	define( 'RPWCGC_URL', plugins_url( 'gift-cards-for-woocommerce', 'giftcards.php' ) );
+
+// Plugin Root File
+if ( ! defined( 'RPWCGC_FILE' ) )
+	define( 'RPWCGC_FILE', plugin_basename( __FILE__ )  );
+
+// Plugin Text Domian
+if ( ! defined( 'RPWCGC_CORE_TEXT_DOMAIN' ) )
+	define( 'RPWCGC_CORE_TEXT_DOMAIN', 'rpgiftcards');
+
+
+function rpgc_woocommerce() {
+
+	if ( !class_exists( 'woocommerce' ) )
+		return;
+
+	if ( is_admin() ) {
+		require_once RPWCGC_PATH . 'admin/giftcard-actions.php';
+		require_once RPWCGC_PATH . 'admin/order-functions.php';
+	}
+
+	require_once RPWCGC_PATH . 'giftcard/giftcard-functions.php';
+	require_once RPWCGC_PATH . 'giftcard/product-functions.php';
+	require_once RPWCGC_PATH . 'giftcard/checkout-functions.php';
+
+	function rpgc_create_post_type() {
+		$show_in_menu = current_user_can( 'manage_woocommerce' ) ? 'woocommerce' : true;
+
+		register_post_type( 'rp_shop_giftcard',
+			array(
+				'labels' => array(
+					'name'      			=> __( 'Gift Cards', RPWCGC_CORE_TEXT_DOMAIN ),
+					'singular_name'			=> __( 'Gift Card', RPWCGC_CORE_TEXT_DOMAIN ),
+					'menu_name'    			=> _x( 'Gift Cards', 'Admin menu name', RPWCGC_CORE_TEXT_DOMAIN ),
+					'add_new'     			=> __( 'Add Gift Card', RPWCGC_CORE_TEXT_DOMAIN ),
+					'add_new_item'    		=> __( 'Add New Gift Card', RPWCGC_CORE_TEXT_DOMAIN ),
+					'edit'      			=> __( 'Edit', RPWCGC_CORE_TEXT_DOMAIN ),
+					'edit_item'    			=> __( 'Edit Gift Card', RPWCGC_CORE_TEXT_DOMAIN ),
+					'new_item'     			=> __( 'New Gift Card', RPWCGC_CORE_TEXT_DOMAIN ),
+					'view'      			=> __( 'View Gift Cards', RPWCGC_CORE_TEXT_DOMAIN ),
+					'view_item'    			=> __( 'View Gift Card', RPWCGC_CORE_TEXT_DOMAIN ),
+					'search_items'    		=> __( 'Search Gift Cards', RPWCGC_CORE_TEXT_DOMAIN ),
+					'not_found'    			=> __( 'No Gift Cards found', RPWCGC_CORE_TEXT_DOMAIN ),
+					'not_found_in_trash'	=> __( 'No Gift Cards found in trash', RPWCGC_CORE_TEXT_DOMAIN ),
+					'parent'     			=> __( 'Parent Gift Card', RPWCGC_CORE_TEXT_DOMAIN )
+				),
+				'public'  		=> true,
+				'has_archive' 	=> true,
+				'show_in_menu'  => $show_in_menu,
+				'hierarchical' 	=> false,
+				'supports'   	=> array( 'title', 'comments' )
+			)
+		);
+	}
+	add_action( 'init', 'rpgc_create_post_type' );
+
+	/**	
+	 * Add the required scripts to the plugin.
+	 *
+	 */
+	function rpgc_enqueue() {
+		global $woocommerce, $post;
+		$rpgc_url = plugins_url() . '/gift-cards-for-woocommerce';
+		wp_enqueue_style( 'rpgc_style', RPWCGC_URL . '/style/style.css' );
+	}
+	add_action( 'wp_enqueue_scripts', 'rpgc_enqueue' );
+
+
+}
+add_action( 'plugins_loaded', 'rpgc_woocommerce', 0 );
---
+++ readme.txt
@@ -0,0 +1,86 @@
+==== Gift Cards for WooCommerce ====
+Contributors: rpletcher
+Tags: woocommerce, gift, gift card, payment, gift certificate, certificate
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 1.3.2
+Donate link: https://ryanpletcher.com/donate/
+License: GPLv2 or later
+
+Gift Cards for WooCommerce allows you to create gift cards that your customers purchase on your site.
+
+== Description ==
+
+Gift Cards for WooCommerce is a plugin which allows you to manage and sell gift certificates for your site. You can sell them to your visitors, and accept payments through any of your other payment gateways.  It will then allow your customers to purchase gift card on the site that you will manually create.
+
+If you have an interest in translating this plugin please let me know.
+
+== Installation ==
+1. Install the WooCommerce Gift Cards plugin
+2. Activate the plugin
+3. Create Giftcard Products (Static price only - must set Giftcard as a single product)
+
+== Future Plans ==
+Make it possible to extend and translate the plugin.
+Form to allow creation of gift cards automatically.
+Bulk creation of gift cards.
+Convert a gift card to a store credit.
+
+If you have a feature that you would like let me know.
+
+== Frequently Asked Questions ==
+
+
+== Changelog ==
+= 1.3.2 =
+* FIX: Gift card fields on all the products
+* FIX: More issues with spelling
+
+= 1.3.1 =
+* FIX: Updated place holder to display the information correctly.
+* FIX: Corrected path on a script to call correct location.
+
+= 1.3 =
+* CHANGE: Changed file stucture of plugin
+* ADD: Gift Card panel in WooCommerce Settings
+* ADD: Ability to determine if your customers can pay for shipping with their giftcard
+* ADD: Setting to determine a default experation date for gift cards purchased online
+* ADD: Documentation for plugin.
+
+= 1.2.5 =
+* FIX: Display of giftcard paymnt on email reciept will now display
+
+= 1.2.4 =
+* FIX: Removed a var_dump accidentally left in the code
+
+= 1.2.3 =
+* FIX: Paypal now will get Giftcard Data on the receipt as a discount
+* FIX: Undefined index on order page when no giftcard is present has been fixed
+
+= 1.2.2 =
+* FIX: Fixed calculation in cart when you change a giftcard on checkout
+
+= 1.2.1 =
+* FIX: Set the sending address for giftcard emails
+* FIX: Removed the addition fields for Giftcards on products that are not
+
+= 1.2 =
+* ADD: Automatic email sent when gift card is created in admin control panel
+* ADD: Check mark option in product to set as a gift card 
+* ADD: Remaining balance of card displayed on receipt
+* ADD: Fields add to gift card product to enter information
+* FIX: Information on gift card on order can not be update
+* FIX: Balance of gift card returned on an order that is refunded
+
+= 1.1 =
+* FIX: Made it possible to edit a gift card after creation
+* FIX: A couple of formating errors
+
+= 1.0 =
+* Initial Release
+
+== Screenshots ==
+None at this time
+
+== Upgrade Notice ==
+None at this time.
