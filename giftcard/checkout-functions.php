05681666e76912db1cedd845b71cc774382133fa
rpletcher <ryan.pletcher@gmail.com>
2014-05-27 19:39:51 -0700

    Add Paypal error fix
    Woocommerce will send a single line item on certain conditions.  I have
    added a check to resolve this issue.

--- giftcard/checkout-functions.php
+++ giftcard/checkout-functions.php
@@ -131,10 +131,18 @@ function rpgc_add_giftcard_to_paypal( $paypal_args ) {
 
 	$giftCardPayment = $woocommerce->session->giftcard_payment;
 
-	if ( isset( $paypal_args['discount_amount_cart'] ) ) {
-		$paypal_args['discount_amount_cart'] = $paypal_args['discount_amount_cart'] + $giftCardPayment;
-	} else { 
-		$paypal_args['discount_amount_cart'] = $giftCardPayment;
+	$custom = unserialize($paypal_args["custom"]);
+	$order = new WC_Order( $custom[0] );
+
+	if ( get_option( 'woocommerce_prices_include_tax' ) == 'yes' || $order->get_order_discount() > 0 || ( sizeof( $order->get_items() ) + sizeof( $order->get_fees() ) ) >= 9 ) {
+		$paypal_args['discount_amount_cart'] = 0;		
+
+	} else {
+		if ( isset( $paypal_args['discount_amount_cart'] ) ) {
+			$paypal_args['discount_amount_cart'] = $paypal_args['discount_amount_cart'] + $giftCardPayment;
+		} else { 
+			$paypal_args['discount_amount_cart'] = $giftCardPayment;
+		}
 	}
 
 	return $paypal_args;
