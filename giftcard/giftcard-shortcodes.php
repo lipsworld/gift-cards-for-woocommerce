fe8c4a372fbd1b4b6d54b4015d2203e65ce5a7d2
rpletcher <ryan.pletcher@gmail.com>
2014-08-11 23:11:19 -0700

    Fixed new short code lookup

--- giftcard/giftcard-shortcodes.php
+++ giftcard/giftcard-shortcodes.php
@@ -16,11 +16,26 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function wpr_check_giftcard( $atts ) {
 	global $wpdb, $woocommerce;
 
-	if ( isset( $_POST['giftcard_code'] ) ) {
+	$giftCardNumber = sanitize_text_field( $_POST['giftcard_code'] );
 
-		var_dump($_POST['giftcard_code'] );
+	$return = '';
 
-		echo '<h3>' . 'Balance is' . '</h3>';
+	$return .= '<form class="check_giftcard_balance" method="post">';
+
+		$return .= '<p class="form-row form-row-first">';
+			$return .= '<input type="text" name="giftcard_code" class="input-text" placeholder="' . __( 'Gift card', RPWCGC_CORE_TEXT_DOMAIN ) . '" id="giftcard_code" value="" />';
+		$return .= '</p>';
+
+		$return .= '<p class="form-row form-row-last">';
+			$return .= '<input type="submit" class="button" name="check_giftcard" value="' . __( 'Check Balance', RPWCGC_CORE_TEXT_DOMAIN ) . '" />';
+		$return .= '</p>';
+
+		$return .= '<div class="clear"></div>';
+	$return .= '</form>';
+	$return .= '<div id="theBalance"></div>';
+
+
+	if ( $_POST['giftcard_code'] ) {
 
 		// Check for Giftcard
 		$giftcard_found = $wpdb->get_var( $wpdb->prepare( "
@@ -31,39 +46,19 @@ function wpr_check_giftcard( $atts ) {
 			AND $wpdb->posts.post_title = '%s'
 		", $giftCardNumber ) );
 
-		if ( $_POST['giftcard_code'] ) {
+		if ( $giftCardNumber ) {
+			$current_date = date("Y-m-d");
+			$cardExperation = get_post_meta( $giftcard_found, 'rpgc_expiry_date', true );
+
 			// Valid Gift Card Entered
 			if ( ( strtotime($current_date) <= strtotime($cardExperation) ) || ( strtotime($cardExperation) == '' ) ) {
 
 				$oldBalance = get_post_meta( $giftcard_found, 'rpgc_balance', true );
-
-				if ( is_string( $oldBalance ) )  // Determin if the Value from $oldBalance is a String and convert it
-					$GiftcardBlance = (float) $oldBalance;
+				$GiftcardBalance = (float) $oldBalance;
 			}
 		}
 
-		return  '<h3>Remaining Balance:</h3> ' . $$_POST['giftcard_code'] ;
-
-	} else {
-
-
-
-		$return = '';
-
-		$return .= '<form class="check_giftcard_balance" method="post">';
-
-			$return .= '<p class="form-row form-row-first">';
-				$return .= '<input type="text" name="giftcard_code" class="input-text" placeholder="' . __( 'Gift card', RPWCGC_CORE_TEXT_DOMAIN ) . '" id="giftcard_code" value="" />';
-			$return .= '</p>';
-
-			$return .= '<p class="form-row form-row-last">';
-				$return .= '<input type="submit" class="button" name="check_giftcard" value="' . __( 'Check Balance', RPWCGC_CORE_TEXT_DOMAIN ) . '" />';
-			$return .= '</p>';
-
-			$return .= '<div class="clear"></div>';
-		$return .= '</form>';
-		$return .= '<div id="theBalance"></div>';
-
+		$return .= '<h3>Remaining Balance: ' . woocommerce_price( $GiftcardBalance ) . '</h3>';
 	}
 
 	return $return;
