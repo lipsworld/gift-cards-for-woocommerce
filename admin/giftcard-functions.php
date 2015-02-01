5126dbd39c73974f330a1c8853cc54665ac554d2
rpletcher <ryan.pletcher@gmail.com>
2015-02-01 14:25:32 -0700

    Updated security and text domain

--- admin/giftcard-functions.php
+++ admin/giftcard-functions.php
@@ -85,7 +85,7 @@ function rpgc_process_giftcard_meta( $post_id, $post ) {
 
 	if( ( ( $sendTheEmail == 1 ) && ( $balance <> 0 ) ) || isset( $_POST['rpgc_resend_email'] ) ) {
 		$blogname 		= wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
-		$subject 		= apply_filters( 'woocommerce_email_subject_gift_card', sprintf( '[%s] %s', $blogname, __( 'Gift Card Information', WPR_CORE_TEXT_DOMAIN ) ), $post->post_title );
+		$subject 		= apply_filters( 'woocommerce_email_subject_gift_card', sprintf( '[%s] %s', $blogname, __( 'Gift Card Information', 'rpgiftcards' ) ), $post->post_title );
 		$sendEmail 		= get_bloginfo( 'admin_email' );
 
 		ob_start();
@@ -95,7 +95,7 @@ function rpgc_process_giftcard_meta( $post_id, $post ) {
 
 		$theMessage 	= apply_filters( 'rpgc_emailContents', $theMessage );
 
-	  	$email_heading 	= __( 'New gift card from ', WPR_CORE_TEXT_DOMAIN ) . $blogname;
+	  	$email_heading 	= __( 'New gift card from ', 'rpgiftcards' ) . $blogname;
 	  	$email_heading 	= apply_filters( 'rpgc_emailSubject', $email_heading );
 
 	  	echo $mailer->wrap_message( $email_heading, $theMessage );
@@ -132,16 +132,16 @@ function sendGiftcardEmail ( $giftCard ) {
 	<div class="message">
 
 
-		<?php _e( 'Dear', WPR_CORE_TEXT_DOMAIN ); ?> <?php echo wpr_get_giftcard_to( $giftCard->ID ); ?>,<br /><br />
+		<?php _e( 'Dear', 'rpgiftcards' ); ?> <?php echo wpr_get_giftcard_to( $giftCard->ID ); ?>,<br /><br />
 			
-		<?php echo wpr_get_giftcard_from( $giftCard->ID ); ?> <?php _e('has selected a', WPR_CORE_TEXT_DOMAIN ); ?> <strong><a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a></strong> <?php _e( 'Gift Card for you! This card can be used for online purchases at', WPR_CORE_TEXT_DOMAIN ); ?> <?php bloginfo( 'name' ); ?>. <br />
+		<?php echo wpr_get_giftcard_from( $giftCard->ID ); ?> <?php _e('has selected a', 'rpgiftcards' ); ?> <strong><a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a></strong> <?php _e( 'Gift Card for you! This card can be used for online purchases at', 'rpgiftcards' ); ?> <?php bloginfo( 'name' ); ?>. <br />
 
-		<h4><?php _e( 'Gift Card Amount', WPR_CORE_TEXT_DOMAIN ); ?>: <?php echo woocommerce_price( wpr_get_giftcard_balance( $giftCard->ID ) ); ?></h4>
-		<h4><?php _e( 'Gift Card Number', WPR_CORE_TEXT_DOMAIN ); ?>: <?php echo $giftCard->post_title; ?></h4>
+		<h4><?php _e( 'Gift Card Amount', 'rpgiftcards' ); ?>: <?php echo woocommerce_price( wpr_get_giftcard_balance( $giftCard->ID ) ); ?></h4>
+		<h4><?php _e( 'Gift Card Number', 'rpgiftcards' ); ?>: <?php echo $giftCard->post_title; ?></h4>
 
 		<?php
 		if ( $expiry_date != "" ) {
-			echo __( 'Expiration Date', WPR_CORE_TEXT_DOMAIN ) . ': ' . date_i18n( get_option( 'date_format' ), strtotime( $expiry_date ) );
+			echo __( 'Expiration Date', 'rpgiftcards' ) . ': ' . date_i18n( get_option( 'date_format' ), strtotime( $expiry_date ) );
 		}
 		?>
 	</div>
@@ -151,12 +151,12 @@ function sendGiftcardEmail ( $giftCard ) {
 	</div>
 
 	<div style="padding-top: 10px; border-top: 1px solid #ccc;">
-		<?php _e( 'Using your Gift Card is easy', WPR_CORE_TEXT_DOMAIN ); ?>:
+		<?php _e( 'Using your Gift Card is easy', 'rpgiftcards' ); ?>:
 
 		<ol>
-			<li><?php _e( 'Shop at', WPR_CORE_TEXT_DOMAIN ); ?> <?php bloginfo( 'name' ); ?></li>
-			<li><?php _e( 'Select "Pay with a Gift Card" during checkout.', WPR_CORE_TEXT_DOMAIN ); ?></li>
-			<li><?php _e( 'Enter your card number.', WPR_CORE_TEXT_DOMAIN ); ?></li>
+			<li><?php _e( 'Shop at', 'rpgiftcards' ); ?> <?php bloginfo( 'name' ); ?></li>
+			<li><?php _e( 'Select "Pay with a Gift Card" during checkout.', 'rpgiftcards' ); ?></li>
+			<li><?php _e( 'Enter your card number.', 'rpgiftcards' ); ?></li>
 		</ol>
 	</div>
 
