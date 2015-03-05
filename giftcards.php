0bb98f81cb157e3496ab79b26539608b5ed87c42
rpletcher <ryan.pletcher@gmail.com>
2015-03-04 23:13:46 -0700

    Updating Woo Nag to includ update and activate

--- giftcards.php
+++ giftcards.php
@@ -209,11 +209,40 @@ class WPRWooGiftcards {
 	 * @return void
 	 */
 	public function no_woo_nag() {
-		?>
-		<div class="updated">
-			<p><?php printf( __( 'WooCommerce - Gift Cards requires that you have WooCommerce Installed and Activated. <a href="%s">Activate Now</a>.', 'rpgiftcards' ), admin_url( 'plugins.php' ) ); ?></p>
-		</div>
-		<?php
+		 // We need plugin.php!
+		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		
+		$plugins = get_plugins();
+		
+		// Set plugin directory
+		$plugin_path = array_filter( explode( '/', $plugin_path ) );
+		$this->plugin_path = end( $plugin_path );
+		
+		// Set plugin file
+		$this->plugin_file = $plugin_file;
+		
+		// Set plugin name
+		$this->plugin_name = 'WooCommerce - Gift Cards';
+		
+		// Is EDD installed?
+		foreach( $plugins as $plugin_path => $plugin ) {
+			var_dump($plugin['Name'] );
+			if( $plugin['Name'] == 'WooCommerce' ) {
+				$this->has_woo = true;
+				$this->wpr_base = $plugin_path;
+				break;
+			}
+		}
+
+        if( $this->has_woo ) {
+            $url  = esc_url( wp_nonce_url( admin_url( 'plugins.php?action=activate&plugin=' . $this->wpr_base ), 'activate-plugin_' . $this->wpr_base ) );
+            $link = '<a href="' . $url . '">' . __( 'activate it', 'rpgiftcards' ) . '</a>';
+        } else {
+            $url  = esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=woocommerce' ), 'install-plugin_woocommerce' ) );
+            $link = '<a href="' . $url . '">' . __( 'install it', 'rpgiftcards' ) . '</a>';
+        }
+        
+        echo '<div class="error"><p>' . $this->plugin_name . sprintf( __( ' requires WooCommerce! Please %s to continue!', 'rpgiftcards' ), $link ) . '</p></div>';
 	}
 
 }
